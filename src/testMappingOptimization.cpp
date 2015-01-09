8a7105a9c6f5d38160ef54e47f40140338b2ba13
Ashish Vaswani <vaswani@hpc-login3.hpcc.usc.edu>
2015-01-08 16:20:18 -0800

    added some mkl

--- src/testMappingOptimization.cpp
+++ src/testMappingOptimization.cpp
@@ -18,6 +18,7 @@ void readWeights(string &embeddings_filename, Eigen::MatrixBase<Derived> &const_
 
 int main (int argc, char *argv[]) 
 {
+	setup_threads(8);
 	Matrix<double,Dynamic,Dynamic> source_embeddings,target_embeddings;
 	source_embeddings.setZero(5001,25);
 	target_embeddings.setZero(5001,25);
